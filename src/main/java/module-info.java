6b8f76e559a7bc910932c1da4ccac5c39551ac2c
AbitVeseli <abit.veseli04@gmail.com>
2025-04-15 12:36:39 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module org.example.g12_storybasedgame {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    requires javafx.graphics;
     opens org.example.g12_storybasedgame to javafx.fxml;
     exports org.example.g12_storybasedgame;
 }
