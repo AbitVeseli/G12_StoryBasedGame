cdeb007178d0f376a81e5296ca86f684af72bfa7
KevinJeppsson <kevinkristerjeppsson@gmail.com>
2025-06-04 11:49:19 +0200

    Update Homescreen.java

--- src/main/java/org/example/g12_storybasedgame/view/homescreen/Homescreen.java
+++ src/main/java/org/example/g12_storybasedgame/view/homescreen/Homescreen.java
@@ -105,7 +105,7 @@ public class Homescreen extends Application {
         bottomBar.setStyle("-fx-alignment: center; -fx-background-color: transparent;");
         bottomBar.setPadding(new Insets(0));
 
-        String[] buttonLabels = {"StoryLine", "Visit", "Play"};
+        String[] buttonLabels = {"Visit", "Play"};
 
         Region leftSpacer = new Region();
         HBox.setHgrow(leftSpacer, Priority.ALWAYS);
